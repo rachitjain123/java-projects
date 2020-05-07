a7bf6378c6fa834fc3e9d5bf047b377cf750dfd6
Rachit Jain <jainrachit100@gmail.com>
2020-05-07 23:03:43 +0530

    Added spring secure app

---
+++ secureApp/src/main/java/com/rjrks/secureapp/User.java
@@ -0,0 +1,36 @@
+package com.rjrks.secureapp;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Id
+    private int id;
+    private String username;
+    private String password;
+}
---
+++ secureApp/src/main/java/com/rjrks/secureapp/UserRepository.java
@@ -0,0 +1,7 @@
+package com.rjrks.secureapp;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+    User findByUsername(String username);
+}
